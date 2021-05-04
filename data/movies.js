d879f4c523c5e1969102aca2aabf84c56155cc0a
Austin Summerlin <summerlin.austin@gmail.com>
2021-05-04 12:02:14 -0700

    Create Horror Movie Data

---
+++ data/movies.js
@@ -0,0 +1,61 @@
+// example seed data
+const movies = [
+  {
+    name: 'Dawn of the Dead',
+    sub_genre: 'Zombie',
+    year: 1978,
+    director: 'George A. Romero',
+    country: 'US'
+  },
+  {
+    name: 'Suspiria',
+    sub_genre: 'Giallo',
+    year: 1977,
+    director: 'Dario Argento',
+    country: 'Italy'
+  },
+  {
+    name: 'Friday The 13th',
+    sub_genre: 'Slasher',
+    year: 1980,
+    director: 'Sean S. Cunningham',
+    country: 'US'
+  },
+  {
+    name: 'Nightmare On Elm Street ',
+    sub_genre: 'Slasher',
+    year: 1984,
+    director: 'Wes Craven',
+    country: 'US'
+  },
+  {
+    name: 'A Girl Walks Home Alone At Night',
+    sub_genre: 'Vampire',
+    year: 2014,
+    director: 'Ana Lily Amirpour',
+    country: 'Iran'
+  },
+  {
+    name: 'Let The Right One In',
+    sub_genre: 'Vampire',
+    year: 2008,
+    director: 'Tomas Alfredson',
+    country: 'Sweeden'
+  },
+  {
+    name: 'The Babadook',
+    sub_genre: 'Supernatural',
+    year: 2014,
+    director: 'Jennifer Kent',
+    country: 'Australia'
+  },
+  {
+    name: 'Cannibal Holocaust',
+    sub_genre: 'Giallo',
+    year: 1980,
+    director: 'Ruggero Deodato',
+    country: 'Italy'
+  }
+];
+
+export default movies;
