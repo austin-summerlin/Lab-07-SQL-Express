0643fa498a279b81a941aaee38ebbe4ab511edb1
Austin Summerlin <summerlin.austin@gmail.com>
2021-05-04 12:11:45 -0700

    add length to data

--- data/movies.js
+++ data/movies.js
@@ -5,56 +5,65 @@ const movies = [
     sub_genre: 'Zombie',
     year: 1978,
     director: 'George A. Romero',
-    country: 'US'
+    country: 'US',
+    length: 2.07
   },
   {
     name: 'Suspiria',
     sub_genre: 'Giallo',
     year: 1977,
     director: 'Dario Argento',
-    country: 'Italy'
+    country: 'Italy',
+    length: 1.32
+
   },
   {
     name: 'Friday The 13th',
     sub_genre: 'Slasher',
     year: 1980,
     director: 'Sean S. Cunningham',
-    country: 'US'
+    country: 'US',
+    length: 1.35
   },
   {
     name: 'Nightmare On Elm Street ',
     sub_genre: 'Slasher',
     year: 1984,
     director: 'Wes Craven',
-    country: 'US'
+    country: 'US',
+    length: 1.31
   },
   {
     name: 'A Girl Walks Home Alone At Night',
     sub_genre: 'Vampire',
     year: 2014,
     director: 'Ana Lily Amirpour',
-    country: 'Iran'
+    country: 'Iran',
+    length: 1.41
   },
   {
     name: 'Let The Right One In',
     sub_genre: 'Vampire',
     year: 2008,
     director: 'Tomas Alfredson',
-    country: 'Sweeden'
+    country: 'Sweeden',
+    length: 1.54
   },
   {
     name: 'The Babadook',
     sub_genre: 'Supernatural',
     year: 2014,
     director: 'Jennifer Kent',
-    country: 'Australia'
+    country: 'Australia',
+    length: 1.34
   },
   {
     name: 'Cannibal Holocaust',
     sub_genre: 'Giallo',
     year: 1980,
     director: 'Ruggero Deodato',
-    country: 'Italy'
+    country: 'Italy',
+    length: 1.35
   }
 ];
 
